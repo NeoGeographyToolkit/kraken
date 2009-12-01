710b64270baebcfd54e28039d1052ca6a8c30aa3
ted <ted@tilefish.local>
2009-11-30 20:10:21 -0800

    jobset creator for scale2int8jobs

--- load_scripts/moc/create_moc_jobs.py
+++ load_scripts/moc/create_moc_jobs.py
@@ -15,13 +15,13 @@ from ngt.utils.tracker import Tracker
 ROOTPATH='/big/assets/mocsource/'
 DESTPATH='/big/assets/moc/'
 
-def generate_jobs(jobset):
+def generate_mocproc_jobs(jobset):
     track = None
     queryset = jobset.assets
     for asset in queryset.get_query_set():
         if not track:
             track = Tracker(target=queryset.count())
-        job = Job(command='moc-stage')
+        job = Job(command=jobset.command)
         sourcefile = asset.file_path
         basename = '/'.join(asset.file_path.split('/')[-2:])
         dest_name = os.path.splitext(basename)[0] + '.cub'
@@ -43,22 +43,34 @@ def generate_jobs(jobset):
         yield job
 
 @transaction.commit_on_success
-def populate_jobs(jobset):
+def populate_mocproc_jobs(jobset):
     for job in Tracker(iter=generate_jobs(jobset), target=jobset.assets.count(), progress=True):
         jobset.jobs.add(job)
-        pass
 
     transaction.commit()
     return jobset.jobs
 
 @transaction.commit_on_success
-def create_jobset():
+def create__mocproc_jobset():
     assets = Asset.objects.filter(class_label='MOC SDP', md5_check=True)
     set = JobSet()
     set.name = 'MOC Staging'
     set.command='moc-stage'
     set.save()
     print "Saved JobSet %d.  Adding assets." % set.id
-    for asset in Tracker(iter=assets.iterator(), target=assets.count(), progress=True):         set.assets.add(asset)
+    for asset in Tracker(iter=assets.iterator(), target=assets.count(), progress=True):
+        set.assets.add(asset)
     transaction.commit()
+    return set
+    
+    
+@transaction.commit_on_successs
+def create_scale2int8_jobset():
+    assets = Asset.objects.filter(class_label='mocprocd image')
+    set = JobSet()
+    set.name = "int8 scaling"
+    set.command = 'scale2int8'
+    set.output_asset_label = "scaled image int8"
+    for asset in Tracker(iter=assets.iterator(), target=assets.count(), progress=True):
+        set.assets.add(asset)
     return set
