9c80e76dd6a110c492db08017cc6b0302492fe51
ted <ted@tilefish.(none)>
2009-12-02 01:26:29 -0800

    Added job feeder as a seperate script. (I surrender).

---
+++ ngt/dispatch/feeder.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import sys, logging, threading, time
+
+sys.path.insert(0, '../..')
+from django.core.management import setup_environ
+from ngt import settings
+setup_environ(settings)
+from models import Reaper
+from ngt.jobs.models import Job, JobSet
+from django.db.models import Q
+
+logger = logging.getLogger('dispatch')
+logger.setLevel(logging.DEBUG)
+
+JOB_MIN = 10
+JOB_MAX = 60
+METER_POLL_INTERVAL = 1 # seconds
+greenlight = threading.Event()
+greenlight.set()
+dblock = threading.Lock()
+
+def enqueue_jobs():
+    ''' Loop and enqueue jobs if the maximum queue size hasn't been exceeded '''
+    logger.debug("Job dispatch is launching.")
+    Job.objects.filter(status='queued').update(status='requeue')
+    while True:
+        active_jobsets = JobSet.objects.filter(active=True)
+        jobset_count = active_jobsets.count()
+        for jobset in active_jobsets:
+            try:
+                greenlight.wait()
+                job = jobset.jobs.filter(Q(status='new') | Q(status='requeue') )[0]
+                dblock.acquire()
+                job.enqueue()
+                dblock.release()
+                time.sleep(0.01)
+            except IndexError:
+                jobset_count -= 1
+                if jobset_count == 0:
+                    logger.info("Ran out of jobs to enqueue. Dispatch thread will exit.")
+                    return True
+                    
+class TrafficLight(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.name = "Traffic Light"
+        
+    def run(self):
+        logger.info("%s starting" % self.name)
+        while True:
+            time.sleep(METER_POLL_INTERVAL)
+            dblock.acquire()
+            running_jobs = Job.objects.filter(status="queued").count()
+            dblock.release()
+            print "%d JOBS QUEUED." % running_jobs
+            if running_jobs >= JOB_MAX:
+                greenlight.clear()
+                logger.info("Light is red.")
+            else:
+                if not greenlight.is_set() and running_jobs <= JOB_MIN:
+                    greenlight.set()
+                    logger.info("Light is green.")
+            
+                    
+if __name__ == '__main__':
+    print "OK go."
+    trafficlight = TrafficLight()
+    trafficlight.start()
+    enqueue_jobs()
