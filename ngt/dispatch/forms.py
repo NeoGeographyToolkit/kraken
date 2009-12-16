ba67fb83817dd116e6a8aa4355e0d67c0953dc15
ted <ted@tilefish.westell.com>
2009-12-16 18:13:48 -0500

    Removes some references to external stuff from the old svn alderaan repo.

--- ngt/dispatch/forms.py
+++ ngt/dispatch/forms.py
@@ -1,7 +1,9 @@
 from django import forms
-from ngt.messaging.amq_config import commands
+#from ngt.messaging.amq_config import commands
+from ngt.dispatch.reaper import Reaper
 from ngt.jobs.models import JobSet
 
+commands = Reaper.commands
 
 class JobForm(forms.Form):
     commandnames = [(k,k) for k in commands.keys()]
