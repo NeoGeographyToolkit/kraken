110d0c26c6410e23e0fdbca5483594e4a365ac3c
ted <ted@tilefish.westell.com>
2009-12-16 18:26:07 -0500

    !!! Deletes the entire pds/ subtree !!!

--- pds/local_settings.py
+++
@@ -1,9 +0,0 @@
-DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-# uses kerberos authentication, make sure to have certificate, run "kinit" command
-DATABASE_NAME = 'wwt_demo'                # Or path to database file if using sqlite3.
-DATABASE_HOST = 'localhost'               # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_USER = 'django'
-DATABASE_PASSWORD = '3djangle'
-DATABASE_PORT = '5432'   
-
-GEOS_LIBRARY_PATH='/usr/lib/libgeos_c.so.1'
