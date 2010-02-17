7ce5adf3a6ba5d3bd5bbcf976e95ea15e567a906
Ted Scharff <e.b.scharff@nasa.gov>
2010-02-17 14:45:23 -0800

    hirise2plate: sets temp dir to /big/tmp/

--- ngt/dispatch/commands/hirise2plate.py
+++ ngt/dispatch/commands/hirise2plate.py
@@ -7,7 +7,7 @@ import math
 import subprocess, shlex
 from subprocess import Popen, PIPE
 
-DEFAULT_TMP_DIR = '/tmp/'
+DEFAULT_TMP_DIR = '/big/tmp/'
 KDU_EXPAND_THREADS = 2
 
 VWBIN_DIR = '/big/software/visionworkbench/bin'
