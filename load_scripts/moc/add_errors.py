82ca96a6c14c5759aed60db117ace82b5b43de5b
Ted Scharff <e.b.scharff@nasa.gov>
2010-02-17 16:22:09 -0800

    Cumulative index didn't do the job. Using individual volume indexes.

--- load_scripts/moc/add_errors.py
+++ load_scripts/moc/add_errors.py
@@ -1,13 +1,29 @@
-import time, os.path
+import time, os.path, glob
 from pds.ingestion.cum_index import Table
 from ngt.utils.tracker import Tracker
 from assets.models import Asset
 
 moc_meta_path = '/big/sourcedata/moc/meta'
+moc_rootpath='/big/assets/mocsource/'
+
 index_files = [
     #('ab1_sp2.lbl', 'ab1_sp2.tab', 2018), # no data_quality_desc field here...
     ('fha_s10.lbl', 'fha_s10.tab', 210078),
 ]
+
+def generate_volnames():
+    for file in glob.glob(os.path.join(moc_rootpath,'mgsc_1*')):
+        if os.path.isdir(file):
+            yield file
+            
+def generate_index_rows():
+    for volname in Tracker(list(generate_volnames()), progress=True):
+        index_path = os.path.join(moc_rootpath, volname, 'index')
+        labelfile = os.path.join(index_path, 'imgindex.lbl')
+        tabfile = os.path.join(index_path, 'imgindex.tab')
+        tbl = Table(labelfile, tabfile)
+        for row in tbl:
+            yield row
 
 def build_index():
     print "Building Index"
@@ -17,7 +33,8 @@ def build_index():
         lbl = os.path.join(moc_meta_path, lbl)
         tab = os.path.join(moc_meta_path, tab)
         table = Table(lbl,tab)
-        for row in Tracker(table, target=count, progress=True):
+        #for row in Tracker(table, target=count, progress=True):
+        for row in generate_index_rows():
             index[row.product_id] = row.data_quality_desc
     print "Finised in %f sec." % (time.time() - t0)
     
