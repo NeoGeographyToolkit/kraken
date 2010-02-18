bc905e420bdc456ff0745df581afe9a0323c0b50
EB Scharff <edward.b.scharff@nasa.gov>
2010-02-17 16:37:42 -0800

    MOC create_mosaic_jobs filters out jobs with errors.

--- load_scripts/moc/create_mosaic_jobs.py
+++ load_scripts/moc/create_mosaic_jobs.py
@@ -36,7 +36,7 @@ def _build_mipmap_jobs(jobset, asset_queryset):
 def create_mipmap_jobs(n_jobs=None, basemap=True):
     # where n_jobs is the number of jobs to generate.  Default (None) builds jobs for all assets in the queryset.
     transaction_id_sequence.setval(1) # reset the transaction_id sequence
-    assets = Asset.objects.filter(class_label='scaled image int8')[:n_jobs]
+    assets = Asset.objects.filter(has_errors=False, class_label='scaled image int8')[:n_jobs]
     jobset = JobSet()
     jobset.name = "Debug MipMap (%s)" % (n_jobs or 'all')
     jobset.command = "mipmap"
