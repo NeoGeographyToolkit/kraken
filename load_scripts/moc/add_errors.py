645650fbcc382a3705c7e4b3f865744c556608b6
EB Scharff <edward.b.scharff@nasa.gov>
2010-02-17 16:16:18 -0800

    Fixes bugs in moc.add_errors.

--- load_scripts/moc/add_errors.py
+++ load_scripts/moc/add_errors.py
@@ -1,11 +1,11 @@
-import time
+import time, os.path
 from pds.ingestion.cum_index import Table
-from ngt.utils import Tracker
+from ngt.utils.tracker import Tracker
 from assets.models import Asset
 
 moc_meta_path = '/big/sourcedata/moc/meta'
 index_files = [
-    ('ab1_sp2.lbl', 'ab1_sp2.tab', 2018),
+    #('ab1_sp2.lbl', 'ab1_sp2.tab', 2018), # no data_quality_desc field here...
     ('fha_s10.lbl', 'fha_s10.tab', 210078),
 ]
 
@@ -14,10 +14,12 @@ def build_index():
     index = {}
     t0 = time.time()
     for lbl, tab, count in index_files:
-        table = Table(lbs,tab)
+        lbl = os.path.join(moc_meta_path, lbl)
+        tab = os.path.join(moc_meta_path, tab)
+        table = Table(lbl,tab)
         for row in Tracker(table, target=count, progress=True):
-            index[row['PRODUCT_ID']] = ROW['DATA_QUALITY_DESC']
-    print "Finised in %f sec." % time.time() - t0
+            index[row.product_id] = row.data_quality_desc
+    print "Finised in %f sec." % (time.time() - t0)
     
     return index
     
@@ -28,10 +30,13 @@ def main():
     print "Fixing %d mocproc'd assets" % mocprocd_assets.count()
     i = 0
     for asset in Tracker(mocprocd_assets, target=mocprocd_assets.count(), progress=True):
-        if index[asset.product_id].strip() == 'ERRORS':
-            asset.has_errors = True
-            asset.save()
-            i += 1
+        try:
+            if index[asset.product_id].strip() == 'ERRORS':
+                asset.has_errors = True
+                asset.save()
+                i += 1
+        except KeyError:
+            print "Error: %s not in index!" % asset.product_id
     print "%d assets have errors" % i
     
     int8_assets = Asset.objects.filter(class_label='scaled image int8')
