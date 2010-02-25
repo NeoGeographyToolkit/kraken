41b6ced38b72910ea7ab467a7dfe591a257c20a7
Ted Scharff <e.b.scharff@nasa.gov>
2010-02-24 18:34:26 -0800

    Snapshot job generation consolidated to one module.

--- load_scripts/hirise/create_mosaic_jobs.py
+++ load_scripts/hirise/create_mosaic_jobs.py
@@ -12,6 +12,7 @@ from ngt.assets.models import Asset
 from ngt.utils.tracker import Tracker
 from ngt.django_extras.db.sequence import Sequence
 from ngt.dispatch.commands.jobcommands import MipMapCommand, hirise2plateCommand, StartSnapshot, EndSnapshot
+from load_scripts.snapshot.create_jobs import create_snapshot_jobs
 
 PLATEFILE = 'pf://wwt10one/index/hirise_v1.plate'
 transaction_id_sequence = Sequence('seq_transaction_id')
@@ -44,76 +45,4 @@ def create_mipmap_jobs(n_jobs=None, basemap=False):
         _build_jobs(MipMapCommand, jobset, Asset.objects.filter(class_label='color basemap'))
     _build_jobs(hirise2plateCommand, jobset, assets)
     return jobset
-        
 
-def _build_snapshot_start_end(transaction_range, jobs_for_dependency, snapshot_jobset, last_endjob):
-    # transaction_id = transaction_id_sequence.nextval() # TODO: this is now wrong.  Should be user-specified, and the range can be inferred
-    transaction_id = transaction_range[1] + 1
-    print "Creating snapshot jobs for transaction range %d --> %d" % transaction_range
-    # create start and end jobs
-    startjob = Job(
-        transaction_id = transaction_id,
-        command = 'start_snapshot',
-        jobset = snapshot_jobset
-    )
-    startjob.arguments = json.dumps(
-        StartSnapshot.build_arguments(
-            startjob, 
-            transaction_range = transaction_range,
-            platefile = PLATEFILE
-        )
-    )
-    
-    endjob = Job(
-        transaction_id = transaction_id,
-        command = 'end_snapshot',
-        jobset = snapshot_jobset
-    )
-    endjob.arguments = json.dumps(EndSnapshot.build_arguments(endjob, platefile=PLATEFILE))
-    #import pdb; pdb.set_trace()
-    startjob.save()
-    endjob.save()
-    # add dependencies
-    print "Adding dependencies."
-    endjob.dependencies.add(startjob)
-    for j in Tracker(iter=jobs_for_dependency, progress=True):
-        startjob.dependencies.add(j)
-    if last_endjob: # initially not set...
-        startjob.dependencies.add(last_endjob)
-    return startjob, endjob
-    
-    
-
-@transaction.commit_on_success   
-def create_snapshot_jobs(mmjobset=None, interval=256):
-    if not mmjobset:
-        mmjobset = JobSet.objects.filter(name__contains="MipMap").latest('pk')
-    snapshot_jobset = JobSet()
-    snapshot_jobset.name = "mosaic snapshots (js%d)" % mmjobset.id
-    snapshot_jobset.command = "snapshot"
-    snapshot_jobset.save()
-
-    i = 0
-    transaction_range_start = None
-    jobs_for_dependency = []
-    endjob = None
-    for mmjob in mmjobset.jobs.all().order_by('transaction_id'):
-        i += 1
-        jobs_for_dependency.append(mmjob)
-        if not transaction_range_start:
-            transaction_range_start = mmjob.transaction_id        
-        if i % interval == 0:
-            transaction_range = (transaction_range_start, mmjob.transaction_id)
-            startjob, endjob = _build_snapshot_start_end(transaction_range, jobs_for_dependency, snapshot_jobset, endjob)
-            #clear transaction range and jobs for dependency list
-            transaction_range_start = mmjob.transaction_id + 1  # Set the start of the next snapshot
-            jobs_for_dependency = []
-    else: # after the last iteration, start a snapshot with whatever's left.
-        if jobs_for_dependency:
-            transaction_range = (transaction_range_start, mmjob.transaction_id)
-            _build_snapshot_start_end(transaction_range, jobs_for_dependency, snapshot_jobset, endjob)
-    print "Setting priority to 1 and activating."
-    snapshot_jobset.priority = 1
-    snapshot_jobset.active = True
-    snapshot_jobset.save()
-    return snapshot_jobset
