db9bc3a1bacf3f1118c78e38daeacc7af8edbdc9
Michael Broxton <michael.broxton@nasa.gov>
2010-03-16 17:57:24 -0700

    Eliminated the need to run kdu_expand.

--- load_scripts/hirise/create_mosaic_jobs.py
+++ load_scripts/hirise/create_mosaic_jobs.py
@@ -14,7 +14,7 @@ from ngt.django_extras.db.sequence import Sequence
 from ngt.dispatch.commands.jobcommands import MipMapCommand, hirise2plateCommand, StartSnapshot, EndSnapshot
 from load_scripts.snapshot.create_jobs import create_snapshot_jobs as _create_snapshot_jobs
 
-PLATEFILE = 'pf://wwt10one/index/hirise_v2.plate'
+PLATEFILE = 'pf://wwt10one/index/hirise_v3.plate'
 transaction_id_sequence = Sequence('seq_transaction_id')
 
 def create_snapshot_jobs(*args, **kwargs):
