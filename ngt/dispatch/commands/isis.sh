4b2712c1d219bf2d1c6396ea4dd5690f1c5b7216
Ted Scharff <e.b.scharff@nasa.gov>
2010-04-22 00:57:19 -0700

    Some command-script cleanup in ISIS land.

--- ngt/dispatch/commands/isis.sh
+++ ngt/dispatch/commands/isis.sh
@@ -7,12 +7,13 @@
 
 if [[ -z $ISISROOT ]]
 then
-    export ISISROOT='/big/packages/isis3/isis'
+    #export ISISROOT='/big/packages/isis3/isis'
     #export ISISROOT='/opt/isis3/isis'
+    export ISISROOT="${HOME}/apps/isis3"
 fi
-echo "ISISROOT is $ISISROOT"
+#echo "ISISROOT is $ISISROOT"
 source "${ISISROOT}/scripts/isis3Startup.sh"
-echo "LD_LIIBRARY_PATH is $LD_LIBRARY_PATH"
+#echo "LD_LIIBRARY_PATH is $LD_LIBRARY_PATH"
 command=$ISISROOT/bin/$1
 shift
 echo "$command $@"
