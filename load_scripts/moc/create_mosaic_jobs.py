d7e4704aa901524129da5f3bdb440388c0b2c082
Ted Scharff <e.b.scharff@nasa.gov>
2010-04-22 16:11:23 -0700

    Updates moc/create_mosaic_jobs.py to use moc2plate.

--- load_scripts/moc/create_mosaic_jobs.py
+++ load_scripts/moc/create_mosaic_jobs.py
@@ -16,35 +16,35 @@ from load_scripts.snapshot.create_jobs import create_snapshot_jobs
 
 
 ROOTPATH='/big/assets/moc/'
-PLATEFILE = 'pf://wwt10one/index/moc_v3.plate'
+PLATEFILE = 'pf://wwt10one/index/moc_v4test.plate'
 transaction_id_sequence = Sequence('seq_transaction_id')
 
 
-def _build_mipmap_jobs(jobset, asset_queryset):
+def _build_mipmap_jobs(jobset, asset_queryset, platefile):
     for asset in Tracker(iter=asset_queryset, target=asset_queryset.count(), progress=True):
         job = Job()
         while True:  # Get the next even transaction ID
             job.transaction_id = transaction_id_sequence.nextval()
             if job.transaction_id % 2 == 0:
                 break
-        job.command = 'mipmap'
-        job.arguments = json.dumps(MipMapCommand.build_arguments(job, platefile=PLATEFILE, file_path=asset.file_path))
+        job.command = 'moc2plate'
+        job.arguments = json.dumps(MipMapCommand.build_arguments(job, platefile=platefile, file_path=asset.file_path))
         job.footprint = asset.footprint
         job.jobset = jobset
         job.save()
         job.assets.add(asset)
         
 @transaction.commit_on_success
-def create_mipmap_jobs(n_jobs=None, basemap=True):
+def create_mipmap_jobs(n_jobs=None, basemap=True, platefile=PLATEFILE):
     # where n_jobs is the number of jobs to generate.  Default (None) builds jobs for all assets in the queryset.
     transaction_id_sequence.setval(1) # reset the transaction_id sequence
-    assets = Asset.objects.filter(has_errors=False, class_label='scaled image int8')[:n_jobs]
+    assets = Asset.objects.filter(has_errors=False, class_label='MOC SDP')[:n_jobs]
     jobset = JobSet()
     jobset.name = "Debug MipMap (%s)" % (n_jobs or 'all')
-    jobset.command = "mipmap"
+    jobset.command = "moc2plate"
     jobset.priority = 3
     jobset.save()
     if basemap:
-        _build_mipmap_jobs(jobset, Asset.objects.filter(class_label='color basemap'))
-    _build_mipmap_jobs(jobset, assets)
+        _build_mipmap_jobs(jobset, Asset.objects.filter(class_label='color basemap'), platefile)
+    _build_mipmap_jobs(jobset, assets, platefile)
     return jobset
