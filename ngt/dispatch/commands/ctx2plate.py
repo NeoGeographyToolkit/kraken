c768e6e17945e103908ec9536e1ad24406959af6
Ted Scharff <e.b.scharff@nasa.gov>
2011-02-17 11:12:24 -0800

    ctx2plate: clipping moved to stretch step

--- ngt/dispatch/commands/ctx2plate.py
+++ ngt/dispatch/commands/ctx2plate.py
@@ -196,16 +196,6 @@ def histeq(incube, outcube):
     finally:
         unlink_if_exists(incube)
 
-def clip_outliers(incube, outcube, n):
-    """
-    Clip all values greater than n standard deviations from the mean.
-    """
-    (old_min, old_max, mean, stdev) = get_stats(incube)
-    new_min = mean - n*stdev
-    new_max = mean + n*stdev
-    args = ('specpix', 'from='+incube, 'to='+outcube, 'LRSMIN=%f'%old_min, 'LRSMAX=%f'%new_min, 'HRSMIN=%f'%new_max, 'HRSMAX=%f'%old_max)
-    isis_run(args, message="Clipping outliers.")
-
 def get_max_camera_latitude(cubefile):
     p = Popen([ os.path.join(COMMAND_PATH, 'isis.sh'), 'camrange', 'from='+cubefile ], stdout=PIPE)
     stats = p.communicate()[0]
@@ -291,9 +281,16 @@ def get_stats(incube):
     return (minimum, maximum, mean, stdev)
 
 
-def stretch2int8(infile, outfile):
-     # stretch from=/home/ted/e1501055.cub to=/home/ted/e1501055_8bit.cub+8bit+0:254 pairs="0.092769:1 0.183480:254" null=0 lis=1 lrs=1 his=255 hrs=255 
+def stretch2int8(infile, outfile, std_deviations=0.0):
+    # stretch from=/home/ted/e1501055.cub to=/home/ted/e1501055_8bit.cub+8bit+0:254 pairs="0.092769:1 0.183480:254" null=0 lis=1 lrs=1 his=255 hrs=255
     (minval, maxval, mean, stdev) = get_stats(infile)
+
+    if (std_deviations > 0.1 ):
+        if (mean-std_deviations*stdev) > minval:
+            minval = mean-std_deviations*stdev
+        if (mean+std_deviations*stdev) < maxval:
+            maxval = mean+std_deviations*stdev
+
     args = (
         'stretch',
         'from='+infile,
@@ -304,11 +301,12 @@ def stretch2int8(infile, outfile):
         'lrs=1',
         'his=255',
         'hrs=255',
-    )
+        )
     try:
         retcode = isis_run(args, message="Converting to int8: %s --> %s" % (infile,outfile))
     finally:
         unlink_if_exists(infile)
+
 
 def image2plate(imagefile, platefile):
     cmd = [IMAGE2PLATE,]
@@ -373,12 +371,11 @@ def ctx2plate(ctxurl, platefile):
     calibrated_cube = os.path.join(options.tmpdir, 'calibrated_'+imgname+'.cub')
     norm_cube = os.path.join(options.tmpdir, 'norm_'+imgname+'.cub')
     bandnorm_cube = os.path.join(options.tmpdir, 'bandnorm_'+imgname+'.cub')
-    clipped_cube = os.path.join(options.tmpdir, 'clipped_'+imgname+'.cub')
     histeq_cube = os.path.join(options.tmpdir, 'histeq_'+imgname+'.cub')
     projected_cube = os.path.join(options.tmpdir, 'projected_'+imgname+'.cub')
     stretched_cube = os.path.join(options.cachedir, 'stretched_'+imgname+'.cub') ### NOTE: This file gets saved to a different location
 
-    if os.path.exists(stretched_cube):
+    if options.caching and os.path.exists(stretched_cube):
         # Shortcut preprocessing and add the stretched cube to plate
         print "File %s already exists!  Skipping preprocessing and running img2plate." % stretched_cube
         if options.write_to_plate and not options.dry_run:
@@ -425,10 +422,6 @@ def ctx2plate(ctxurl, platefile):
             working_cube = reduced_cube
         else:
             working_cube = calibrated_cube # skip downsampling
-        
-        if options.clipping >= 0:
-            clip_outliers(working_cube, clipped_cube, options.clipping)
-            working_cube = clipped_cube
 
         cubenorm(working_cube, norm_cube)
         working_cube = norm_cube
@@ -443,7 +436,7 @@ def ctx2plate(ctxurl, platefile):
 
         map_project(working_cube, projected_cube)
 
-        stretch2int8(projected_cube, stretched_cube)
+        stretch2int8(projected_cube, stretched_cube, options.clipping)
 
         #Delete original tmpfile, if it exists
         if using_tmpfile:
@@ -472,16 +465,17 @@ def main():
     parser = OptionParser(usage=usage)
     parser.add_option('--tmpdir', action='store', dest='tmpdir', help="Where to write intermediate images (default: %s)" % DEFAULT_TMP_DIR)
     parser.add_option('--cachedir', action='store', dest='cachedir', help="Where to write final (pre-plate) images for caching(default: %s)" % DEFAULT_CACHE_DIR)
+    parser.add_option('--nocache', action='store_false', dest='caching', help='Disable caching (force preprocessing even if the output cube exists).')
     parser.add_option('-t', '--transaction-id', action='store', dest='transaction_id', type='int')
     parser.add_option('--preserve', '-p', dest='delete_files', action='store_false', help="Don't delete the intermediate files.")
     parser.add_option('--noplate', dest='write_to_plate', action='store_false', help="Like --dry-run, except run everything but image2plate")
     parser.add_option('--dry-run', dest='dry_run', action='store_true', help='Print the commands to be run without actually running them') # NOTE: --dry-run will always throw errors, because we can't use the isis tools to pull values and stats from intermediate files that don't exist!
     parser.add_option('--downsample', dest='downsample', action='store', type='float', help="Percentage to downsample (as float)")
     parser.add_option('--histeq', dest='histeq', action='store_true', help="Apply histogram equalization", default=False)
-    parser.add_option('c', '--clipping', dest='clipping', action='store', type='float', help="Clip intensity values beyond N standard deviations from the mean. 0 disables. (Default: 2.5) ", default=2.5)
-    parser.add_option('--bandnorm', dest='bandnorm', action='store_true', help="Apply ISIS bandnorm tool.", default = False)
+    parser.add_option('-c', '--clipping', dest='clipping', action='store', type='float', help="Clip intensity values beyond N standard deviations from the mean. 0 disables. (Default 3)", default=3)
+    parser.add_option('--bandnorm', dest='bandnorm', action='store_true', help="Apply ISIS bandnorm tool.")
     parser.add_option('-v', '--verbose', dest='verbose', action='store_true', help='More output!')
-    parser.set_defaults(cachedir=DEFAULT_CACHE_DIR, tmpdir=DEFAULT_TMP_DIR, transaction_id=None, delete_files=True, write_to_plate=True, dry_run=False, verbose=False, downsample=100)
+    parser.set_defaults(caching=True, cachedir=DEFAULT_CACHE_DIR, tmpdir=DEFAULT_TMP_DIR, transaction_id=None, delete_files=True, write_to_plate=True, dry_run=False, verbose=False, bandnorm=False, downsample=100)
     (options, args) = parser.parse_args()
     if options.tmpdir != DEFAULT_TMP_DIR and options.cachedir == DEFAULT_CACHE_DIR:
         options.cachedir = options.tmpdir
