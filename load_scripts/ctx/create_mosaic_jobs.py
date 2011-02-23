3d770ca282b1065896e7aa925ddc1667ae11856d
Ted Scharff <e.b.scharff@nasa.gov>
2011-02-22 22:01:24 -0800

    CTX job creation script updated to use PDS cumulative index for production.

--- load_scripts/ctx/create_mosaic_jobs.py
+++ load_scripts/ctx/create_mosaic_jobs.py
@@ -16,6 +16,7 @@ from ngt.utils.tracker import Tracker
 #from ngt.django_extras.db.sequence import Sequence
 from ngt.dispatch.commands.jobcommands import ctx2plateCommand, StartSnapshot, EndSnapshot
 from load_scripts.snapshot.create_jobs import create_snapshot_jobs
+from pds.ingestion import cum_index
 
 
 METADATA_DIR = '/big/sourcedata/mars/ctx/meta'
@@ -57,19 +58,33 @@ def _build_mipmap_jobs(jobset, urls, platefile, n_jobs=None, options=None):
         if n_jobs and i >= n_jobs: break
     print "Created %d jobs." % i
 
-def generate_urls(metadata_dir=METADATA_DIR, baseurl='http://pds-imaging.jpl.nasa.gov/data/mro/mars_reconnaissance_orbiter/ctx/'):
-    #indextable = os.path.join(metadata_dir, 'cumindex.tab')
-    indextable = os.path.join(metadata_dir, 'ctx_1000.csv')
-    indexReader = csv.reader(open(indextable, 'r'))
-    for row in indexReader:
-        if len(row) < 2: continue # skip blank lines
-        volume, filespec = row[0:2]
-        volume = volume.lower()
-        head, tail = os.path.split(filespec)
-        head = head.lower()
-        url = os.path.join(baseurl, volume, head, tail)
-        yield url
-        
+def generate_urls(metadata_dir=METADATA_DIR, baseurl='http://pds-imaging.jpl.nasa.gov/data/mro/mars_reconnaissance_orbiter/ctx/', is_test=False):
+    if is_test:
+        """ Read from the 1000 image sample around Valles Marineris. """
+        indextable = os.path.join(metadata_dir, 'ctx_1000.csv')
+        indexReader = csv.reader(open(indextable, 'r'))
+        for row in indexReader:
+            if len(row) < 2: continue # skip blank lines
+            volume, filespec = row[0:2]
+            volume = volume.lower()
+            head, tail = os.path.split(filespec)
+            head = head.lower()
+            url = os.path.join(baseurl, volume, head, tail)
+            yield url
+    else:
+        """ Use the PDS Cumulative Index """
+        tablefile = os.path.join(metadata_dir, 'cumindex.tab')
+        labelfile = os.path.join(metadata_dir, 'cumindex.lbl')
+        table = cum_index.Table(labelfile, tablefile)
+        for row in table:
+            if row.data_quality_desc.strip() == 'OK':  # filter for data quality errors
+                volume = row.volume_id.lower()
+                head, tail = os.path.split(row.file_specification_name)
+                head = head.lower()
+                url = os.path.join(baseurl, volume, head, tail)
+                yield url
+
+
 @transaction.commit_on_success
 def create_mipmap_jobs(n_jobs=None, platefile=DEFAULT_PLATEFILE, name=None, options=None):
     # where n_jobs is the number of jobs to generate.  Default (None) builds jobs for all assets in the queryset.
@@ -79,7 +94,7 @@ def create_mipmap_jobs(n_jobs=None, platefile=DEFAULT_PLATEFILE, name=None, opti
     jobset.command = "ctx2plate"
     jobset.priority = 3
     jobset.save()
-    _build_mipmap_jobs(jobset, generate_urls(), platefile, n_jobs=n_jobs, options=options)
+    _build_mipmap_jobs(jobset, generate_urls(is_test=options.test), platefile, n_jobs=n_jobs, options=options)
     return jobset
 
 def main():
@@ -95,6 +110,7 @@ def main():
     parser.add_option('--nocache', action='store_false', dest='use_cache', help='If there is a cached output cube, reprocess anyway')
     parser.add_option('--use-cache', action='store_true', dest='use_cache', help='Use a cached copy of the preprocessed output, if one exists.')
     parser.add_option('--percentages', dest='use_percentages', action='store_true', help="Use percentages instead of values for the stretch step (overrides clipping setting)")
+    parser.add_option('--test', dest='test', action='store_true', help="Draw image urls from a particular set of test images rather than the PDS cumulative label (default).")
     parser.set_defaults(
         platefile = DEFAULT_PLATEFILE, 
         activate = True, 
@@ -106,6 +122,7 @@ def main():
         clipping = 3.0, 
         use_cache = False,
         use_percentages = False,
+        test = False,
     )
     (options, args) = parser.parse_args()
 
