7ab82551ffb146c894e668879aa4279dd2300ef2
EB Scharff <edward.b.scharff@nasa.gov>
2011-06-22 15:10:07 -0700

    Adds --augment mode to CTX create_mosaic_jobs.  This mode allows the loader to add additional imagee2plate jobs to to an existing jobset if new images are released during processing.

--- load_scripts/ctx/create_mosaic_jobs.py
+++ load_scripts/ctx/create_mosaic_jobs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import sys, os, json
 import csv
+import re
+import datetime
 import optparse
 
 sys.path.insert(0, '../..')
@@ -23,13 +25,13 @@ METADATA_DIR = '/big/sourcedata/mars/ctx/meta'
 DEFAULT_PLATEFILE = 'pf://wwt10one/index/test_ctx_default.plate'
 #transaction_id_sequence = Sequence('seq_transaction_id')
 
-def gen_transaction_ids():
-    i = 0
+def gen_transaction_ids(initial=0):
+    i = initial
     while True:
         i += 2
         yield i
 
-def _build_mipmap_jobs(jobset, urls, platefile, n_jobs=None, options=None):
+def _build_mipmap_jobs(jobset, urls, platefile, n_jobs=None, options=None, initial_transaction_id=0, presave_callback=None):
     if options:
         downsample = options.downsample
         bandnorm = options.bandnorm
@@ -38,7 +40,7 @@ def _build_mipmap_jobs(jobset, urls, platefile, n_jobs=None, options=None):
         downsample = None
         bandnorm = False
         clipping = 0
-    transaction_ids = gen_transaction_ids()
+    transaction_ids = gen_transaction_ids(initial_transaction_id)
     i = 0
     for url in Tracker(iter=urls, target=27859, progress=True):
         job = Job()
@@ -55,6 +57,8 @@ def _build_mipmap_jobs(jobset, urls, platefile, n_jobs=None, options=None):
             job.arguments.append('--noplate')
         job.arguments.append('--clipping=%f' % clipping)
         job.jobset = jobset
+        if presave_callback:
+            job = presave_callback(job)
         job.save()
         i += 1
         if n_jobs and i >= n_jobs: break
@@ -100,6 +104,45 @@ def create_mipmap_jobs(n_jobs=None, platefile=DEFAULT_PLATEFILE, name=None, opti
     _build_mipmap_jobs(jobset, generate_urls(is_test=options.test), platefile, n_jobs=n_jobs, options=options)
     return jobset
 
+@transaction.commit_on_success
+def augment_jobset(jobset_id, platefile=DEFAULT_PLATEFILE, options=None):
+    assert not options.test
+
+    print "Retrieving JobSet %d..." % jobset_id
+    jobset = JobSet.objects.get(id=jobset_id)
+
+    print "Querying last transaction_id...",
+    last_transaction_id = jobset.jobs.latest("transaction_id").transaction_id
+    print str(last_transaction_id)
+
+    print "Collecting product_ids already in jobset %d..." % jobset_id,
+    existing_product_ids = set()
+    existing_count = 0
+    for job in jobset.jobs.only("context"):
+        prod_id = job.arguments[0][-30:-4]
+        existing_product_ids.add(prod_id)
+        existing_count += 1
+    print "done (%d jobs exist)." % existing_count
+
+    def filter_urls(urls, id_set):
+        product_id_pattern = re.compile('[A-Z0-9]{3}_\d{6}_\d{4}_X[IN]_\d{2}[NS]\d{3}W')
+        for url in urls:
+            product_id = product_id_pattern.search(url).group(0)
+            assert product_id
+            if product_id not in id_set:
+                yield url
+
+    new_urls = filter_urls(generate_urls(), existing_product_ids)
+
+    def presave(job):
+        job.context['aftermarket'] = True
+        job.context['creation_date'] = str(datetime.date.today())
+        return job
+    
+    _build_mipmap_jobs(jobset, new_urls, platefile, options=options, initial_transaction_id=last_transaction_id, presave_callback=presave)
+    print "Added %d new jobs." % ( jobset.jobs.count() - existing_count )
+    return jobset
+
 def main():
     parser = optparse.OptionParser()
     parser.add_option('-p', '--platefile', action='store', dest='platefile', help='Platefile URL to which the images should be written (e.g. pf://wwt10one/index/collectible.plate)')
@@ -115,6 +158,7 @@ def main():
     parser.add_option('--percentages', dest='use_percentages', action='store_true', help="Use percentages instead of values for the stretch step (overrides clipping setting)")
     parser.add_option('--noplate', dest='no_plate', action='store_true', help="Skip platefile insertion.  Just preprocess.")
     parser.add_option('--test', dest='test', action='store_true', help="Draw image urls from a particular set of test images rather than the PDS cumulative label (default).")
+    parser.add_option('--augment', dest='augment', action='store', type='int', help="Augment an existing jobset by creating new jobs only for products that do not already exist within the jobset.  Requires a jobset ID.")
     parser.set_defaults(
         platefile = DEFAULT_PLATEFILE, 
         activate = True, 
@@ -127,11 +171,18 @@ def main():
         use_cache = False,
         use_percentages = False,
         test = False,
+        augment = None,
         no_plate = False,
     )
     (options, args) = parser.parse_args()
 
-    mm_jobset = create_mipmap_jobs(n_jobs=options.n_jobs, platefile=options.platefile, name=options.jobset_name, options=options)
+    if options.augment:
+        options.test = False
+        options.activate = False
+        options.do_snapshots = False
+        augment_jobset(options.augment, platefile=options.platefile, options=options)
+    else:
+        mm_jobset = create_mipmap_jobs(n_jobs=options.n_jobs, platefile=options.platefile, name=options.jobset_name, options=options)
     if options.do_snapshots:
         sn_jobset = create_snapshot_jobs(mmjobset=mm_jobset, platefile=options.platefile)
     else:
